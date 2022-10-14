65c73527c1aa4686ac054f1332d3d80f42bbcafb
Gregor Riepl <gregor.riepl@swisstxt.ch>
2022-10-14 13:59:34 +0200

    Remove superfluous escapes and add URL printing

--- entrypoint.sh
+++ entrypoint.sh
@@ -84,14 +84,14 @@ for path in ${PATHS}; do
   fullpath="${GITHUB_WORKSPACE}/${path}"
   echo "::notice::Processing path ${fullpath}"
   find ${fullpath} -type f -exec curl \
-    --write-out "\{} %{speed_upload}B/s %{size_upload}B %{response_code}\n" \
+    --write-out "%{url} %{speed_upload}B/s %{size_upload}B %{response_code}\n" \
     --silent \
     --show-error \
     --location \
     --fail-with-body \
     --header "${AUTH_HEADER}" \
     --header "Content-Type: application/octet-stream" \
-    --data-binary @"\{}" \
+    --data-binary @"{}" \
     "https://uploads.github.com/repos/${GITHUB_REPOSITORY}/releases/${RELEASE_ID}/assets?name=$(basename \{})" \
   \;
 done
